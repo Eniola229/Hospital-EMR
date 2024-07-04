5ebc2f17b361805714c56a37e4daa43c27ee642e
Adeyemi Joshua <joshuaadeyemi445@gmail.com>
2024-07-04 20:06:48 +0100

    did a little update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -33,6 +33,7 @@
     <link rel="stylesheet" href="{{ asset('css/notifications.css') }}" />
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 
     <!--[if lt IE 9]>
@@ -176,7 +177,7 @@
                                 @else
                                     <div class="list-group">
                                         @foreach($refers as $refer)
-                                        <a href="">
+                                        <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" style="text-align: left; border: none; cursor: pointer; box-shadow: none;">
                                             <div class="card notification-card">
                                                 <div class="card-body">
                                                     <div class="notification-header">
@@ -191,7 +192,27 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                        </a>
+                                        </button>
+
+                                        <!-- Modal for each message -->
+                                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                          <div class="modal-dialog">
+                                            <div class="modal-content">
+                                              <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Vew Message</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                              </div>
+                                              <div class="modal-body">
+                                                ...
+                                              </div>
+                                              <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+
                                         @endforeach
                                     </div>
                                 @endif
@@ -321,7 +342,7 @@
     <!-- custom js -->
     <script src="{{ asset('js/custom.js') }}"></script>
     <script src="{{ asset('js/chart_custom_style1.js') }}"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
